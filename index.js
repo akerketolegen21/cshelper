5578dbf7847389d900fad5406642f251f5f43753
akerketolegen21 <akerketolegen.at@gmail.com>
2020-05-19 23:38:51 +0600

    First few command samples

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ require('dotenv').config()
 const token = process.env.BOT_TOKEN
 const bot = new Telegraf(token)
 
-bot.on('text', ctx => ctx.reply("Hey, we're here to help our fellow CS students with course and career info! Let's start! Привет, мы хотим помочь студентам CS с курсами и поделиться другими полезными материалами! Начнем!"))
-
+bot.start('text', ctx => ctx.reply("Hey, ${ctx.from.first_name} we're here to help our fellow CS students with course and career info! Let's start! Привет, мы хотим помочь студентам CS с курсами и поделиться другими полезными материалами! Начнем!"))
+bot.help(ctx => ctx.reply("Here's our small FAQ section"))
 bot.launch()
