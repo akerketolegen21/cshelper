a13a637a6cb6ac9a68d9fbfb86646ea7906f7a17
akerketolegen21 <akerketolegen.at@gmail.com>
2020-07-30 20:03:15 +0600

    multi file proj + course name, code functions

---
+++ courses.js
@@ -0,0 +1,37 @@
+const { keyboard } = require("telegraf/markup");
+
+const coreCourses = ['CSCI 151: Programming for Scientists and Engineers', 'CSCI 152: Performance and Data Structures', 
+'CSCI 231: Computer Systems and Organizations', 'CSCI 235: Programming Languages',  'CSCI 270: Algorithms', 
+'CSCI 272: Formal Languages', 'ROBT 206: Microcontrollers with Lab',
+'CSCI 307: Research Methods', 'CSCI 332: Operating Systems', 'CSCI 333: Computer Networks', 
+'CSCI 341: Database Systems', 'CSCI  361: Software Engineering', 
+'CSCI 390: Artificial Intelligence', 'CSCI 408: Senior Project I', 'CSCI 409: Senior Project II', 'Back']
+
+const otherCsCourses = ['CSCI 245: Systems Analysis and Design', 'CSCI 262: Software Project Management', 'CSCI 281: Human-Computer Interaction',
+'CSCI 281: Human-Computer Interaction', 'CSCI 325: High Performance Computing', 'CSCI 330: Mobile Computing',
+'CSCI 331: Advanced Mobile Computing', 'CSCI 336: Ubiquity and Sensing', 'Back'
+]
+
+const initList = ['Core courses info', 'Other CS Courses', 'Non-CS Technical electives', 'Natural science electives', 'Back']
+
+const courseKeyboard = [['Syllabus', 'Tips', 'Web-site', 'Prof']]
+function splitStr(str) { 
+      
+    // Function to split string 
+    var string = str.split(" "); 
+      
+    return Array<string> ans;
+} 
+
+function getCurrentCourse(message) {
+    let course = splitStr(message)[0];
+    return course;
+}
+function getCurrentCode(message){
+    let code = splitStr(message)[1];
+
+    return code;
+}
+exports.coreCourses = coreCourses; exports.otherCsCourses = otherCsCourses; exports.initList = initList;
+exports.courseKeyboard = courseKeyboard;
+exports.getCurrentCode = getCurrentCode; exports.getCurrentCourse = getCurrentCourse;
--- index.js
+++ index.js
@@ -2,35 +2,28 @@ const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
 require('dotenv').config()
+var Courses = require('./courses')
 
 const token = process.env.BOT_TOKEN
 const bot = new Telegraf(token)
 
-let coreCourses = ['CSCI 151: Programming for Scientists and Engineers', 'CSCI 152: Performance and Data Structures', 
-'CSCI 231: Computer Systems and Organizations', 'CSCI 235: Programming Languages',  'CSCI 270: Algorithms', 
-'CSCI 272: Formal Languages', 'ROBT 206: Microcontrollers with Lab',
-'CSCI 307: Research Methods', 'CSCI 332: Operating Systems', 'CSCI 333: Computer Networks', 
-'CSCI 341: Database Systems', 'CSCI  361: Software Engineering', 
-'CSCI 390: Artificial Intelligence', 'CSCI 408: Senior Project I', 'CSCI 409: Senior Project II']
-
+var currentCourseName, currentCourseCode;
+var steps = 0;
 bot.use(async (ctx, next) => {
     const start = new Date()
     await next()
     const ms = new Date() - start
     console.log('Response time: %sms', ms)
-  })
+})
+bot.catch((err, ctx) => {
+    console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)})
 
-
-  bot.catch((err, ctx) => {
-    console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
-  })
 bot.start((ctx) => ctx.reply("Hey, "+ctx.message.from.first_name+", we're here to help our fellow CS students with course and career info! Let's start! Привет, мы хотим помочь студентам CS с курсами и поделиться другими полезными материалами! Начнем!"))
 
 bot.help(ctx => ctx.reply("Here's our small FAQ section"))
 
-bot.command('learn', ctx => ctx.reply('Choose on what course you want info', Markup.keyboard(
-    ['Core courses info', 'Other CS Courses', 'Non-CS Technical electives', 'Natural science electives']
-    )
+bot.command('learn', ctx => ctx.reply('Choose on what course you want info', 
+    Markup.keyboard(Courses.initList)
     .resize()
     .extra()
 )
@@ -39,27 +32,39 @@ bot.command('stop', (ctx) => {
     ctx.reply(':)', Extra.markup((m) => m.removeKeyboard()))
   })
 
-bot.hears('Core courses info', ctx => ctx.reply('Choose on what course you want info', 
-    Markup.keyboard(coreCourses)
+bot.hears('Core courses info', ctx => {
+  currentCourseName = Courses.getCurrentCourse(ctx.session.lastMessage);
+  currentCourseCode = Courses.getCurrentCode(ctx.session.lastMessage);
+  return ctx.reply('Choose on what course you want info', 
+    Markup.keyboard(Courses.coreCourses)
     .resize()
-    .extra()))
+    .extra())
+}
+)
 
-bot.hears('Other CS Courses', ctx => ctx.reply('Choose on what course you want info', Markup.keyboard(
-    ['CSCI 245: Systems Analysis and Design', 'CSCI 262: Software Project Management', 'CSCI 281: Human-Computer Interaction',
-    'CSCI 281: Human-Computer Interaction', 'CSCI 325: High Performance Computing', 'CSCI 330: Mobile Computing',
-    'CSCI 331: Advanced Mobile Computing', 'CSCI 336: Ubiquity and Sensing'
-    ])
+
+bot.hears('Other CS Courses', ctx => {
+  return ctx.reply('Choose on what course you want info', Markup.keyboard(
+    Courses.otherCsCourses)
     .resize()
     .extra()
-    ))
+    ) }
+    )
 
 bot.hears("CSCI 151: Programming for Scientists and Engineers", ctx => ctx.reply('Choose' , 
-Markup.keyboard(['151 Syllabus', '151 Tips', '151 Web-site', '151 Prof'])
-.resize()
+    Markup.keyboard(Courses.courseKeyboard).resize()
     .extra()
     ))
+
 bot.hears("151 Syllabus", ctx=> ctx.replyWithHtml(ctx.chat.id, "Sign in or sign up to this site to see the official website", Extra.Markup.inlineKeyboard([
     Markup.urlButton('Syllabus: Course Info', 'https://sst-csci.com/csci151/syllabus/about-the-course/'), 
     Markup.callbackButton('Delete', 'delete')
 ])))
+
+
+
 bot.launch()
+
+// TODO: after choosing the course, each time parse the string to save current course, course code
+// to find further like this switch 'CSCI': case '151', case '152' etc.
+// TODO: going back
